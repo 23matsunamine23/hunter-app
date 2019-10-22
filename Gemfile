a27bc721e18d4228f8778d4f3f425791cb1ac396
23matsunamine23 <noname0323namine@gmail.com>
2019-10-22 23:26:56 +0900

    create chart

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'haml-rails'
 gem 'font-awesome-rails'
 
 gem "chartkick"
+
+gem 'chartable'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,5 @@
 class ContentsController < ApplicationController
   def index
+    @chart = {"2018'-10-1" => 10, "2018-10-02" => 20, "2018-10-03" => 20, "2018-10-04" => 20, "2018-10-05" => 20, "2018-10-06" => 20}
   end
 end
